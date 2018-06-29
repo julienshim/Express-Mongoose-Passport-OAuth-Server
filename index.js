a49f22255c9d6089aa0748b98dabf437bb1c9fe6
jerauld <25723428+jerauld@users.noreply.github.com>
2018-06-29 15:41:36 -0700

    added basic server set up linked to mongod, basic page routes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require ('./config/keys');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 //set up view engine
 app.set('view engine', 'ejs');
+
+//connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log('connected to mongodb');
+});
 
 //set up routes
 app.use('/auth', authRoutes);
@@ -13,6 +23,6 @@ app.get('/', (req, res)=> {
     res.render('home');
 });
 
-app.listen(3000, () => {
-    console.log('app now listening for request on port 3000');
+app.listen(PORT, () => {
+    console.log('App is now listening for request on http://localhost:'+ PORT);
 })
