93a75941b2d3fc2ba54849d81d1b3a7eb5cd0b50
jerauld <25723428+jerauld@users.noreply.github.com>
2018-06-29 16:01:24 -0700

    added initial cookie stuff but haven't tested stuff just for proof of concept

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const mongoose = require('mongoose');
 const keys = require ('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //set up view engine
 app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+    //max age of cookie 24 hours * 60 minutes * 60 seconds * 1000 millisecond
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+
+//intialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
